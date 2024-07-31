f3478922d7e03ea5afb3605b41d3d85a8ff57275
Jallah Sumbo <j.sumbo@alustudent.com>
2024-07-31 13:43:24 +0200

    Trigger Heroku rebuild

--- mylms/settings.py
+++ mylms/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['edukpreneur-8af9fe026a40.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
